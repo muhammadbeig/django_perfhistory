e421bed2f2497be0265583a70c5f5940adeda690
Saud Beig <saud.beig@gmail.com>
2015-10-01 11:41:01 -0700

    minor changes to views and templates

--- myapp/models.py
+++ myapp/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
         project_description = models.CharField(max_length=400)
 
         def __unicode__(self):
-                return u'%s %s' % (self.project_name, self.id)
+                return u'%d|%s|%s' % (self.id, self.project_name, self.project_description)
 
 class Tags(models.Model):
 		project = models.ForeignKey(Project)
--- myapp/views.py
+++ myapp/views.py
@@ -6,4 +6,4 @@ from myapp.models import Project, Tags
 def index(request):
 	context = RequestContext(request)
 	allprojs= Project.objects.all()
-    	return render_to_response('index.html', { 'an_attrib': allprojs }, context)
+	return render_to_response('index.html', { 'projects_all': allprojs }, context)
