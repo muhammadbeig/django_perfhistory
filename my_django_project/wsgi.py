a5468f92b2c9a52755e4d4564b20de04cac0d147
Saud Beig <saud.beig@gmail.com>
2016-01-29 12:31:15 -0800

    added migrations

--- my_django_project/wsgi.py
+++ my_django_project/wsgi.py
@@ -15,3 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_django_project.settings")
 
 application = get_wsgi_application()
+
+# application =  django.core.handlers.wsgi.WSGIHandler()
