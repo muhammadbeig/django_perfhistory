727a466f5ce0ed3db4481ae957fefb6a27801adc
Saud Beig <saud.beig@gmail.com>
2016-02-24 19:39:51 -0800

    added conditioning in the add_time filter so that it doesn't fail with invalid start time or duration

--- perfhistory/templatetags/add_time.py
+++ perfhistory/templatetags/add_time.py
@@ -5,4 +5,9 @@ register = template.Library()
 @register.filter(name='add_time')
 def add_time(input_time, offset_minutes):
 	# print input_time, offset_minutes
+	if not input_time:
+		return ""
+	if not offset_minutes:
+		return input_time
+
 	return  input_time + datetime.timedelta(minutes=offset_minutes)
